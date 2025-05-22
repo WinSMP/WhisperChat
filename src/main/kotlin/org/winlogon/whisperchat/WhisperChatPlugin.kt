d7db12a6c536da5119bc8ad701e501d9073e5a5d
winlogon <walker84837@gmail.com>
2025-05-22 13:09:27 +0200

    chore: sort some imports

--- src/main/kotlin/org/winlogon/whisperchat/WhisperChatPlugin.kt
+++ src/main/kotlin/org/winlogon/whisperchat/WhisperChatPlugin.kt
@@ -16,10 +16,11 @@ import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
-import net.kyori.adventure.text.format.NamedTextColor
+
 import io.papermc.paper.event.player.AsyncChatEvent
 
 import java.util.*
