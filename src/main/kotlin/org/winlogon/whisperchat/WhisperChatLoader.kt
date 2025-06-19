cfca382aff915c87f5333878e6af9413967c05b5
winlogon <walker84837@gmail.com>
2025-06-19 22:06:30 +0200

    chore: move dependency management to paper loader

---
+++ src/main/kotlin/org/winlogon/whisperchat/WhisperChatLoader.kt
@@ -0,0 +1,37 @@
+package org.winlogon.scalaloader
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder
+import io.papermc.paper.plugin.loader.PluginLoader
+import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver
+
+import org.eclipse.aether.artifact.DefaultArtifact
+import org.eclipse.aether.graph.Dependency
+import org.eclipse.aether.repository.RemoteRepository
+
+class ScalaPluginLoader : PluginLoader {
+    override fun classloader(classpathBuilder: PluginClasspathBuilder) {
+        val resolver = MavenLibraryResolver()
+
+        val repositories = mapOf(
+            "central" to "https://repo.maven.apache.org/maven2/",
+            "winlogon-code" to "https://maven.winlogon.org/releases/",
+        )
+
+        for ((name, url) in repositories) {
+            resolver.addRepository(
+                RemoteRepository.Builder(name, "default", url).build()
+            )
+        }
+
+        val dependencies = mapOf(
+            "org.winlogon:retrohue" to "0.1.0",
+        )
+
+        for ((artifact, version) in dependencies) {
+            val dependency = Dependency(DefaultArtifact("$artifact:$version"), null)
+            resolver.addDependency(dependency)
+        }
+
+        classpathBuilder.addLibrary(resolver)
+    }
+}
